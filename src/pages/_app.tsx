f6fbba2221e05cf46b48deb14ca5cead5f0f936e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-09 19:10:37 +0900

    feat: :lipstick: GlobalStyle 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
+  <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
-// v1/api/alchols 2990 + 10
-
-// useInfiniteQuery()
-// intersectionObserver

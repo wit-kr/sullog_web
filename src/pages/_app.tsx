93d4b1ad9807522099bb495051dc6e5d31892877
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-11 00:15:58 +0900

    GlobalStyle html font-size 62.5% 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from 'next/app';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
   body {
     margin: 0;
     padding: 0;

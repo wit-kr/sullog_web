837812805a688352aa3762152e2a45536c1d3502
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-10 15:27:30 +0900

    네이버 버튼 컴포넌트 생성

---
+++ src/components/login/button/naver/index.tsx
@@ -0,0 +1,10 @@
+import { Container, Icon } from './styles';
+
+const NaverButton = () => (
+  <Container>
+    네이버 로그인
+    <Icon src="/image/naver.svg" alt="naver" />
+  </Container>
+);
+
+export default NaverButton;
---
+++ src/components/login/button/naver/styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 330px;
+  height: 48px;
+  background-color: #03c75a;
+  border-radius: 6px;
+  font-size: 16px;
+  color: #fff;
+`;
+
+export const Icon = styled.img`
+  position: absolute;
+  left: 20px;
+`;

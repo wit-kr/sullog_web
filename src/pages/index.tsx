d70f46e20d07d88b454ec11a54c062e117d0f15f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-12 00:44:40 +0900

    feat: :lipstick: css 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import type { NextPage } from 'next';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -16,50 +15,46 @@ const Wrapper = styled.div`
   @supports (-webkit-touch-callout: none) {
     height: -webkit-fill-available;
   }
-  > p {
-    position: absolute;
-    bottom: 7.75vh;
-    text-align: center;
+`;
+
+const Description = styled.p`
+  margin-top: 16px;
+  padding-bottom: 7.75vh;
+  text-align: center;
+  font-family: 'NotoSansKR';
+  color: #bbb;
+  font-size: 1.1rem;
+  font-weight: 350;
+  line-height: 1.6rem;
+  button {
     font-family: 'NotoSansKR';
-    color: #bbb;
     font-size: 1.1rem;
-    font-weight: 350;
-    line-height: 1.6rem;
-    button {
-      font-family: 'NotoSansKR';
-      font-size: 1.1rem;
-      font-weight: bold;
-      color: #bbbbbb;
-      border: none;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      appearance: none;
-      background: none;
-      padding: 0;
-      background-color: transparent;
-    }
+    font-weight: bold;
+    color: #bbbbbb;
+    border: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    background: none;
+    padding: 0;
+    background-color: transparent;
   }
 `;
 
 const Logo = styled.img`
-  position: absolute;
-  top: 35vh;
-  /* margin-top: 35vh; */
+  margin-top: 35vh;
 `;
 
 const Slogan = styled.img`
-  position: absolute;
-  top: 42.5vh;
-  /* margin-top: 1.5vh; */
+  margin-top: 12px;
 `;
 
 const ButtonContainer = styled.div`
-  position: absolute;
   display: flex;
   flex-direction: column;
-  bottom: 14.75vh;
-  > div {
-    margin-bottom: 1vh;
+  margin-top: 28.75vh;
+  > div:first-child {
+    margin-bottom: 8px;
   }
 `;
 
@@ -71,14 +66,14 @@ const Home: NextPage = () => (
       <KakaoButton />
       <NaverButton />
     </ButtonContainer>
-    <p>
+    <Description>
       첫 로그인시&nbsp;
       <button type="button">이용약관</button>
       &nbsp;및
       <br />
       <button type="button">개인정보처리방침</button>
       &nbsp;동의로 간주됩니다.
-    </p>
+    </Description>
   </Wrapper>
 );
 

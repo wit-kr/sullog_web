123a7ed612b84b12405c435d9a1deb1d0bb7c1d2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-13 00:19:10 +0900

    feat: :sparkles: _document.tsx 생성 및 초기화

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => (
-  <Html>
-    <Head />
+  <Html lang="kr">
+    <Head>
+      <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+    </Head>
     <body>
       <Main />
       <NextScript />

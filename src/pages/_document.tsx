88b77821f2a70b1c6369040fd5ae2a8c45389b75
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-13 00:21:45 +0900

    fix: :fire: viewport 태그는 _document.tsx에서 사용하지 말라고 함

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => (
   <Html lang="kr">
-    <Head>
-      <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
-    </Head>
+    <Head />
     <body>
       <Main />
       <NextScript />

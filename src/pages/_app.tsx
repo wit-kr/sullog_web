9691de272a8874ca8b6c66f3c3044323f5e35911
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-06-13 13:25:35 +0900

    feat: :sparkles: meta 태그 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from 'styles/GlobalStyle';
 import '@public/fonts/style.css';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>sullog</title>
+    </Head>
     <GlobalStyle />
     <Component {...pageProps} />
   </>

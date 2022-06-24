308c0dc11fb76bd136b718ffb7eae0ed85539e81
jinho1011 <gino9940@gmail.com>
2022-06-25 00:46:02 +0900

    🔐 chore: dev 모드에서 NEXT_PUBLIC으로 env 접근

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,27 +1,27 @@
 import Map from 'react-map-gl';
-import { MAPBOX_TOKEN } from 'shared/key';
 
-const mapboxAccessToken =
-  process.env.NODE_ENV === 'development'
-    ? MAPBOX_TOKEN
-    : process.env.MAPBOX_TOKEN;
-
-const Home = () => (
-  <h1>
-    <Map
-      initialViewState={{
-        longitude: 126.977966,
-        latitude: 37.566536,
-        zoom: 14,
-      }}
-      style={{
-        width: 600,
-        height: 400,
-      }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={mapboxAccessToken}
-    />
-  </h1>
-);
+const Home = () => {
+  const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN?.replace(
+    / /g,
+    ''
+  );
+  return (
+    <h1>
+      <Map
+        initialViewState={{
+          longitude: 126.977966,
+          latitude: 37.566536,
+          zoom: 14,
+        }}
+        style={{
+          width: 600,
+          height: 400,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={mapboxAccessToken}
+      />
+    </h1>
+  );
+};
 
 export default Home;

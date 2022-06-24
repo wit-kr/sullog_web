40085dad0a543ececc821fc04ffb770d92ab04d0
jinho1011 <gino9940@gmail.com>
2022-06-24 11:04:32 +0900

    ✨ feat: add MapBox in home

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,3 +1,22 @@
-const Home = () => <h1>Home</h1>;
+import Map from 'react-map-gl';
+import { MAPBOX_TOKEN } from 'shared/key';
+
+const Home = () => (
+  <h1>
+    <Map
+      initialViewState={{
+        longitude: 126.977966,
+        latitude: 37.566536,
+        zoom: 14,
+      }}
+      style={{
+        width: 600,
+        height: 400,
+      }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    />
+  </h1>
+);
 
 export default Home;

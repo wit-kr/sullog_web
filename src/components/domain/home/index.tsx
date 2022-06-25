f3cfcecfd4f01da29327f7c124e56188be5f0c0e
jinho1011 <gino9940@gmail.com>
2022-06-25 16:43:36 +0900

    ✨ feat: change map language

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,4 +1,6 @@
-import Map from 'react-map-gl';
+import { useCallback, useRef } from 'react';
+import ReactMapGL, { MapRef } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Home = () => {
   const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN?.replace(
@@ -6,19 +8,38 @@ const Home = () => {
     ''
   );
 
+  const mapRef = useRef<MapRef>(null);
+
+  const onMapLoad = useCallback(() => {
+    if (mapRef.current) {
+      const map = mapRef.current.getMap();
+      map.getStyle().layers.forEach((layer) => {
+        if (layer?.layout && layer?.layout['text-field']) {
+          map.setLayoutProperty(layer.id, 'text-field', [
+            'coalesce',
+            ['get', 'name_ko'],
+            ['get', 'name'],
+          ]);
+        }
+      });
+    }
+  }, []);
+
   return (
     <h1>
-      <Map
+      <ReactMapGL
+        ref={mapRef}
+        onLoad={onMapLoad}
         initialViewState={{
           longitude: 126.977966,
           latitude: 37.566536,
-          zoom: 14,
+          zoom: 3,
         }}
         style={{
-          width: 600,
+          width: '100vw',
           height: 400,
         }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/mapbox/light-v10"
         mapboxAccessToken={mapboxAccessToken}
       />
     </h1>

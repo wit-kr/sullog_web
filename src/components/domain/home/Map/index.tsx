3eb2b613fe4929891f869cea1c67f64249a4bf58
jinho1011 <gino9940@gmail.com>
2022-07-02 18:04:10 +0900

    ✨ feat: 동적으로 레이어 변경

--- src/components/domain/home/Map/index.tsx
+++ src/components/domain/home/Map/index.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useRef, useCallback, useState, useEffect } from 'react';
-import { Layer, LngLatBoundsLike } from 'mapbox-gl';
+import { LngLatBoundsLike, Style } from 'mapbox-gl';
 import ReactMapGL, { MapRef } from 'react-map-gl';
-import { fromJS } from 'immutable';
-
 import MAP_STYLE from './map-style-light-v10.json';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -12,32 +10,73 @@ const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN?.replace(
   ''
 );
 
-const defaultMapStyle: any = fromJS(MAP_STYLE);
+const maxBounds = [
+  {
+    lat: 32.671017,
+    lng: 125.801356,
+  },
+  {
+    lat: 38.724702,
+    lng: 129.693625,
+  },
+];
+
+const categories = [
+  'labels',
+  'roads',
+  'buildings',
+  'parks',
+  'water',
+  'background',
+];
+
+const layerSelector: {
+  [key: string]: RegExp;
+} = {
+  background: /background/,
+  water: /water/,
+  parks: /park/,
+  buildings: /building/,
+  roads: /bridge|road|tunnel/,
+  labels: /label|place|poi/,
+};
+
+const visibility: {
+  [key: string]: boolean;
+} = {
+  water: true,
+  parks: true,
+  buildings: true,
+  roads: true,
+  labels: true,
+  background: true,
+};
 
 const Map = () => {
-  const maxBounds = [
-    {
-      lat: 32.671017,
-      lng: 125.801356,
-    },
-    {
-      lat: 38.724702,
-      lng: 129.693625,
-    },
-  ];
+  const [mapStyle, setMapStyle] = useState(MAP_STYLE);
+
+  useEffect(() => {
+    const { layers } = mapStyle;
+    const modifiedLayers = layers.filter((layer) => {
+      const { id } = layer;
+      return categories.every(
+        (name) => visibility[name] || !layerSelector[name].test(id)
+      );
+    });
+  }, [mapStyle]);
 
   return (
     <ReactMapGL
       initialViewState={{
         longitude: 126.977966,
-        latitude: 37.566536,
-        zoom: 3,
+        latitude: 37.516536,
+        zoom: 10,
       }}
       style={{
         width: '100vw',
         height: '100vh',
       }}
-      mapStyle={defaultMapStyle}
+      mapStyle={mapStyle as Style}
       mapboxAccessToken={mapboxAccessToken}
       maxBounds={maxBounds as LngLatBoundsLike}
     />

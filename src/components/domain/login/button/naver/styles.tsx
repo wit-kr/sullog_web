94ac940d5ed924e9013e743e237452dac2b8942f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-07-05 15:57:05 +0900

    :sparkles: div -> button 태그로 변경

--- src/components/domain/login/button/naver/styles.tsx
+++ src/components/domain/login/button/naver/styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.button`
   display: flex;
   position: relative;
   flex-direction: row;

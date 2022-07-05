9993f6dda6cf7302e05c97753f08edd0abdf1d4a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-07-05 15:59:54 +0900

    :lipstick: 색상코드변경

--- src/components/domain/login/styles.tsx
+++ src/components/domain/login/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  background-color: #3E14B4;
+  background-color: #3e14b4;
   display: flex;
   flex-direction: column;
   align-items: center;

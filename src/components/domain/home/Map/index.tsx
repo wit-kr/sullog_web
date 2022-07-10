cb1ae5d1848a78fe5574d939c1487e9ca833e0c0
jinho1011 <gino9940@gmail.com>
2022-07-10 21:30:51 +0900

    💄 chore: container 스타일 추가

--- src/components/domain/home/Map/index.tsx
+++ src/components/domain/home/Map/index.tsx
@@ -92,7 +92,7 @@ const Map = () => {
         zoom: 10,
       }}
       style={{
-        width: '100vw',
+        width: '100%',
         height: '100vh',
       }}
       mapStyle={mapStyle as Style}

0065893715b06c4932c9ccf94b5145158fd3adc8
jinho1011 <gino9940@gmail.com>
2022-07-19 16:42:32 +0900

    ✨ feat: locate user to naver login page

--- src/components/domain/login/button/naver/index.tsx
+++ src/components/domain/login/button/naver/index.tsx
@@ -1,7 +1,7 @@
 import { Container, Icon } from './styles';
 
 const NaverButton = () => (
-  <Container>
+  <Container href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=D_1pjm3y2CmU8A7NfcnD&state=SULLOG_NAVER_LOGIN&redirect_uri=http://52.78.33.186:8080/sullog/login/naverLoginCallback">
     <Icon src="/image/naver.svg" alt="naver" />
     네이버 로그인
   </Container>
--- src/components/domain/login/button/naver/styles.tsx
+++ src/components/domain/login/button/naver/styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.button`
+export const Container = styled.a`
   display: flex;
   position: relative;
   flex-direction: row;

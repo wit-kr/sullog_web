18e74292fd5325449f689f6677b53c44d55484b5
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-08-03 10:45:37 +0900

    검색 모달 버그 수정 (#37)
    * :bento: searchIcon.svg
    
    * :sparkles: autofocus 추가
    
    * :sparkles: 화면 벗어나는 UI 버그 수정
    
    * :sparkles: 유저가 페이지를 줌하지 못하도록 수정
    
    * :lipstick: box-shadow 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,7 +28,7 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
         />
         <meta
           name="viewport"
-          content="viewport-fit=cover, width=device-width, initial-scale=1.0"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
         <title>sullog</title>
       </Head>

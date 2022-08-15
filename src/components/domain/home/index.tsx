96361d308ace570f3d0f50a0c33fac27746bb558
jinho1011 <gino9940@gmail.com>
2022-08-15 15:16:16 +0900

    Remove unused styled

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,17 +1,6 @@
 import styled from 'styled-components';
 import Map from './Map';
 
-const Container = styled.div`
-  margin: 0px auto;
-  background-color: rgb(255, 255, 255);
-  width: min(500px, 100%);
-  min-height: 100vh;
-`;
-
-const Home = () => (
-  <div>
-    <Map />
-  </div>
-);
+const Home = () => <Map />;
 
 export default Home;

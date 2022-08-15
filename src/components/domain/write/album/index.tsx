2dd5ef4269c47ba0d37474c0d0bea385c7f257c5
jinho1011 <gino9940@gmail.com>
2022-08-15 16:24:20 +0900

    Add album page

---
+++ src/components/domain/write/album/index.tsx
@@ -0,0 +1,3 @@
+const Album = () => <></>;
+
+export default Album;
---
+++ src/pages/write/album.tsx
@@ -0,0 +1,6 @@
+import Album from '@components/domain/write/album';
+import type { NextPage } from 'next';
+
+const AlbumPage: NextPage = () => <Album />;
+
+export default AlbumPage;

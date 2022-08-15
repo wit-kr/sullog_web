d4fe5aaf9ca716bcfa2f121ac617f316bb0f931b
jinho1011 <gino9940@gmail.com>
2022-08-15 14:46:37 +0900

    Fix styles

--- src/components/domain/login/button/kakao/index.tsx
+++ src/components/domain/login/button/kakao/index.tsx
@@ -1,10 +1,14 @@
-import { Container, Icon } from './styles';
+import { Button, Icon } from '../../styles';
 
 const KakaoButton = () => (
-  <Container>
+  <Button
+    color="#181600"
+    backgroundColor="#fee500"
+    href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=D_1pjm3y2CmU8A7NfcnD&state=SULLOG_NAVER_LOGIN&redirect_uri=http://52.78.33.186:8080/sullog/login/naverLoginCallback"
+  >
     <Icon src="/image/kakao.svg" alt="kakao" />
     카카오톡 로그인
-  </Container>
+  </Button>
 );
 
 export default KakaoButton;
--- src/components/domain/login/button/naver/index.tsx
+++ src/components/domain/login/button/naver/index.tsx
@@ -1,10 +1,14 @@
-import { Container, Icon } from './styles';
+import { Button, Icon } from '../../styles';
 
 const NaverButton = () => (
-  <Container href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=D_1pjm3y2CmU8A7NfcnD&state=SULLOG_NAVER_LOGIN&redirect_uri=http://52.78.33.186:8080/sullog/login/naverLoginCallback">
+  <Button
+    color="#ffffff"
+    backgroundColor="#03c75a"
+    href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=D_1pjm3y2CmU8A7NfcnD&state=SULLOG_NAVER_LOGIN&redirect_uri=http://52.78.33.186:8080/sullog/login/naverLoginCallback"
+  >
     <Icon src="/image/naver.svg" alt="naver" />
     네이버 로그인
-  </Container>
+  </Button>
 );
 
 export default NaverButton;

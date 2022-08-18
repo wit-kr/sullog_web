79dc3b728259bd495131451afe163caa0d260be0
jinho1011 <gino9940@gmail.com>
2022-08-19 00:36:31 +0900

    Add AuthProvider

---
+++ src/components/shared/Auth/AuthProvider.tsx
@@ -0,0 +1,78 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { getStorage, STORAGE_KEY } from '../../../shared/storage';
+
+export interface User {
+  token: string;
+  email: string;
+  type: 'naver' | 'kakao';
+}
+
+export class Auth {
+  user: User | null;
+
+  constructor() {
+    this.user = null;
+  }
+
+  resolveUser() {
+    if (window) {
+      const savedUser = getStorage(STORAGE_KEY.USER);
+      if (savedUser) {
+        this.user = {
+          token: savedUser.token,
+          email: savedUser.email,
+          type: savedUser.type,
+        };
+      }
+    } else {
+      this.user = null;
+    }
+
+    return this.user;
+  }
+}
+
+export const AuthContext = createContext<
+  | {
+      auth: Auth;
+      user: User | null;
+      initializing: boolean;
+    }
+  | undefined
+>(undefined);
+
+export function useAuth() {
+  const auth = useContext(AuthContext);
+
+  if (!auth) {
+    throw new Error('useAuth must be used within AuthProvider');
+  }
+
+  return auth;
+}
+
+const auth = new Auth();
+
+export function AuthProvider({ children }: { children: JSX.Element }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const user = auth.resolveUser();
+
+    if (user) {
+      setUser(user);
+    } else {
+      setUser(null);
+    }
+    setInitializing(false);
+  }, []);
+
+  const value = {
+    user,
+    auth,
+    initializing,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}

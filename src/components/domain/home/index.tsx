2696cd99f65dc1e97389acd912025525c2c3a3c7
jinho1011 <gino9940@gmail.com>
2022-08-19 00:58:41 +0900

    Remove unused imports

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -2,7 +2,6 @@
 import { useRouter } from 'next/router';
 import { instance } from 'pages/_app';
 import { useEffect, useLayoutEffect } from 'react';
-import { getStorage, KEY, setStorage } from 'shared/storage';
 import Map from './Map';
 
 const Home = () => {

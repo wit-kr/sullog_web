a6a0cd53990d1a5425fd189e1acd7c08ebaf3d38
jinho1011 <gino9940@gmail.com>
2022-08-19 15:15:38 +0900

    Fix lint errors

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,15 +1,10 @@
 /* eslint-disable camelcase */
-import { useRouter } from 'next/router';
-import { instance } from 'pages/_app';
-import { useEffect, useLayoutEffect } from 'react';
 import Map from './Map';
 
-const Home = () => {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <Map />
+  </div>
+);
 
 export default Home;

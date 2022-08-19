da12f864aae6fb33114690dfb1e7dccf59f3a49e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-08-19 16:04:41 +0900

    :sparkles: 여러가지 컴포넌트들 생성

--- src/components/domain/search/modal/index.tsx
+++ src/components/domain/search/modal/index.tsx
@@ -1,11 +1,19 @@
 import {
   Container,
+  ContentsWrapper,
   RecentContentsWrapper,
   RecentContentsTitleBox,
   RecentContentsTitle,
   DeleteAllButton,
+  NoResultWrapper,
+  RecentItemsBox,
+  RecentItems,
+  DeleteButton,
+  DeleteButtonIcon,
 } from '@components/domain/search/modal/styles';
 import HeaderWithBack from '@components/layout/header/headerWithBack';
+import NoResult from '@components/domain/search/noResult';
+import Item from '@components/domain/search/item';
 
 type modalProps = {
   isModalShow: boolean;
@@ -15,11 +23,43 @@ type modalProps = {
 const SearchModal = ({ isModalShow, setIsModalShow }: modalProps) => (
   <Container>
     <HeaderWithBack isModalShow={isModalShow} setIsModalShow={setIsModalShow} />
+    <ContentsWrapper>
+      <Item />
+    </ContentsWrapper>
+    {/* <NoResultWrapper> */}
+    {/*   <NoResult /> */}
+    {/* </NoResultWrapper> */}
     {/* <RecentContentsWrapper> */}
     {/*   <RecentContentsTitleBox> */}
     {/*     <RecentContentsTitle>최근 검색어</RecentContentsTitle> */}
     {/*     <DeleteAllButton>모두 지우기</DeleteAllButton> */}
     {/*   </RecentContentsTitleBox> */}
+    {/*   <RecentItemsBox> */}
+    {/*     <RecentItems> */}
+    {/*       맛있는 술 */}
+    {/*       <DeleteButton> */}
+    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
+    {/*       </DeleteButton> */}
+    {/*     </RecentItems> */}
+    {/*     <RecentItems> */}
+    {/*       달달한 과실주 */}
+    {/*       <DeleteButton> */}
+    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
+    {/*       </DeleteButton> */}
+    {/*     </RecentItems> */}
+    {/*     <RecentItems> */}
+    {/*       맛있고 달달한 술 */}
+    {/*       <DeleteButton> */}
+    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
+    {/*       </DeleteButton> */}
+    {/*     </RecentItems> */}
+    {/*     <RecentItems> */}
+    {/*       숙취 별로 없는 막걸리 */}
+    {/*       <DeleteButton> */}
+    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
+    {/*       </DeleteButton> */}
+    {/*     </RecentItems> */}
+    {/*   </RecentItemsBox> */}
     {/* </RecentContentsWrapper> */}
   </Container>
 );
---
+++ src/components/domain/search/noResult/styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-family: 'NotoSansKRRegular';
+  font-size: 1.4rem;
+  line-height: 2.6rem;
+  color: #b0b0b0;
+`;

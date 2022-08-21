6d8f328197333470ec7f34156ffd822d775feca0
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-08-21 16:40:52 +0900

    :sparkles: 검색 결과 삭제 및 저장구현

--- src/components/domain/search/modal/index.tsx
+++ src/components/domain/search/modal/index.tsx
@@ -1,67 +1,50 @@
 import {
   Container,
   ContentsWrapper,
-  RecentContentsWrapper,
-  RecentContentsTitleBox,
-  RecentContentsTitle,
-  DeleteAllButton,
   NoResultWrapper,
-  RecentItemsBox,
-  RecentItems,
-  DeleteButton,
-  DeleteButtonIcon,
 } from '@components/domain/search/modal/styles';
 import HeaderWithBack from '@components/layout/header/headerWithBack';
 import NoResult from '@components/domain/search/noResult';
 import Item from '@components/domain/search/item';
+import { useEffect, useState } from 'react';
+import RecentContents from '@components/domain/search/recentContents';
+import { is } from '@babel/types/lib/index-legacy';
 
 type modalProps = {
   isModalShow: boolean;
   setIsModalShow: (isModalShow: boolean) => void;
 };
 
-const SearchModal = ({ isModalShow, setIsModalShow }: modalProps) => (
-  <Container>
-    <HeaderWithBack isModalShow={isModalShow} setIsModalShow={setIsModalShow} />
-    <ContentsWrapper>
-      <Item />
-    </ContentsWrapper>
-    {/* <NoResultWrapper> */}
-    {/*   <NoResult /> */}
-    {/* </NoResultWrapper> */}
-    {/* <RecentContentsWrapper> */}
-    {/*   <RecentContentsTitleBox> */}
-    {/*     <RecentContentsTitle>최근 검색어</RecentContentsTitle> */}
-    {/*     <DeleteAllButton>모두 지우기</DeleteAllButton> */}
-    {/*   </RecentContentsTitleBox> */}
-    {/*   <RecentItemsBox> */}
-    {/*     <RecentItems> */}
-    {/*       맛있는 술 */}
-    {/*       <DeleteButton> */}
-    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
-    {/*       </DeleteButton> */}
-    {/*     </RecentItems> */}
-    {/*     <RecentItems> */}
-    {/*       달달한 과실주 */}
-    {/*       <DeleteButton> */}
-    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
-    {/*       </DeleteButton> */}
-    {/*     </RecentItems> */}
-    {/*     <RecentItems> */}
-    {/*       맛있고 달달한 술 */}
-    {/*       <DeleteButton> */}
-    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
-    {/*       </DeleteButton> */}
-    {/*     </RecentItems> */}
-    {/*     <RecentItems> */}
-    {/*       숙취 별로 없는 막걸리 */}
-    {/*       <DeleteButton> */}
-    {/*         <DeleteButtonIcon src="/image/icon/delete.svg" /> */}
-    {/*       </DeleteButton> */}
-    {/*     </RecentItems> */}
-    {/*   </RecentItemsBox> */}
-    {/* </RecentContentsWrapper> */}
-  </Container>
-);
+const SearchModal = ({ isModalShow, setIsModalShow }: modalProps) => {
+  const [searchArr, setSearchArr] = useState<string[]>(
+    JSON.parse(localStorage.getItem('search')!)
+  );
+  const [isSubmit, setIsSubmit] = useState<boolean>(false);
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  useEffect(() => {
+    setSearchArr(JSON.parse(localStorage.getItem('search')!));
+  }, [isSubmit]);
+  return (
+    <Container>
+      <HeaderWithBack
+        isModalShow={isModalShow}
+        setIsModalShow={setIsModalShow}
+        isSubmit={isSubmit}
+        setIsSubmit={setIsSubmit}
+        isFocus={isFocus}
+        setIsFocus={setIsFocus}
+      />
+      <ContentsWrapper>
+        {!isSubmit ? (
+          <RecentContents searchArr={searchArr} setSearchArr={setSearchArr} />
+        ) : (
+          <NoResultWrapper>
+            <NoResult />
+          </NoResultWrapper>
+        )}
+      </ContentsWrapper>
+    </Container>
+  );
+};
 
 export default SearchModal;
---
+++ src/components/domain/search/recentContents/recentItem/styles.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const RecentItems = styled.li`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  font-family: 'NotoSansKRRegular';
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 2.6rem;
+  color: #545454;
+  margin-bottom: 4px;
+`;
+
+export const DeleteButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const DeleteButtonIcon = styled.img``;

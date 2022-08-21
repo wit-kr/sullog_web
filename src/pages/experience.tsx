70fbc587834c572c4ab336beb90b4816d33d5da3
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-08-21 23:44:56 +0900

    :sparkles: 플레이버 슬라이더바 제외 경험화면 UI 구현

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next';
 import Experience from '@components/domain/experience';
+import { ReactElement } from 'react';
+import Layout from '@components/layout';
+import { NextPageWithLayout } from './_app';
 
 const ExperiencePage: NextPage = () => <Experience />;
 

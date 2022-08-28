428951c1839c3e4efebeaaf4a14e05229a068c92
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-08-28 12:40:47 +0900

    :bug: 빌드 오류 수정

--- src/components/domain/search/modal/index.tsx
+++ src/components/domain/search/modal/index.tsx
@@ -42,9 +42,7 @@ const SearchModal = ({ isModalShow, setIsModalShow }: modalProps) => {
           <RecentContents searchArr={searchArr} setSearchArr={setSearchArr} />
         </RecentContentsWrapper>
       ) : (
-        <ResultContentsWrapper>
-          <Result />
-        </ResultContentsWrapper>
+        <ResultContentsWrapper>{/* <Result /> */}</ResultContentsWrapper>
       )}
     </Container>
   );

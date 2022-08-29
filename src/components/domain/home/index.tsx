d50753391429efa0662b8084cdea06802e3e598a
jinho1011 <gino9940@gmail.com>
2022-08-29 14:32:06 +0900

    Replace to allExp

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable camelcase */
 import Layout from '@components/layout';
+import { useGetAllExp } from 'hooks/useGetAllExp';
 import Map from './Map';
 
-const Home = () => (
-  <Layout>
-    <Map />
-  </Layout>
-);
+const Home = () => {
+  const userRecords = useGetAllExp();
+
+  if (userRecords.isError) {
+    return <div>ERROR</div>;
+  }
+
+  if (userRecords.isLoading || userRecords.isIdle) {
+    return <div>Loading,,</div>;
+  }
+
+  console.log(userRecords.data.data[0]);
+
+  return (
+    <Layout>
+      <Map records={userRecords.data.data} />
+    </Layout>
+  );
+};
 
 export default Home;

d5f482952de71f672ff2b23c7822f25a71eac0fa
jinho1011 <gino9940@gmail.com>
2022-08-29 17:20:21 +0900

    Add route to experience

--- src/components/layout/slider/card/index.tsx
+++ src/components/layout/slider/card/index.tsx
@@ -13,30 +13,44 @@ import {
   TopTitleBox,
   Wrapper,
 } from '@components/layout/slider/card/styles';
+import { useRouter } from 'next/router';
 import { Experience } from 'types/sullog.interface';
 
-const Card = ({ record }: { record: Experience }) => (
-  <Wrapper>
-    <TopTitleBox>
-      <PinImage src="/image/icon/pin.svg" alt="pin" />
-      <Direction>
-        {record.province} {record.city}
-      </Direction>
-      <Category>
-        {record.type === '소주/증류주' ? '소주' : record.type}
-      </Category>
-    </TopTitleBox>
-    <BottomBox>
-      <LeftBox>
-        <TitleBox>
-          <DrinkTitle>{record.name}</DrinkTitle>
-          <BrandTitle>{record.manufacturer}</BrandTitle>
-        </TitleBox>
-        <Description>{record.etc}</Description>
-      </LeftBox>
-      <RightBox />
-    </BottomBox>
-  </Wrapper>
-);
+const Card = ({ record }: { record: Experience }) => {
+  const router = useRouter();
+  return (
+    <Wrapper
+      onClick={() => {
+        router.push({
+          pathname: '/experience/[userSeq]/[alcholSeq]',
+          query: {
+            userSeq: record.user_seq,
+            alcholSeq: record.alchol_seq,
+          },
+        });
+      }}
+    >
+      <TopTitleBox>
+        <PinImage src="/image/icon/pin.svg" alt="pin" />
+        <Direction>
+          {record.province} {record.city}
+        </Direction>
+        <Category>
+          {record.type === '소주/증류주' ? '소주' : record.type}
+        </Category>
+      </TopTitleBox>
+      <BottomBox>
+        <LeftBox>
+          <TitleBox>
+            <DrinkTitle>{record.name}</DrinkTitle>
+            <BrandTitle>{record.manufacturer}</BrandTitle>
+          </TitleBox>
+          <Description>{record.etc}</Description>
+        </LeftBox>
+        <RightBox />
+      </BottomBox>
+    </Wrapper>
+  );
+};
 
 export default Card;
